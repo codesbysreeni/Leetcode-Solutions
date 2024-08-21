437883ee71f08cc852a871a2ebda75f6175a717d
Sreenivas Balasubramanian <sreenivas.balasubramanian@gmail.com>
2024-08-20 20:05:05 -0700

    Add files via upload

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,5 +1,22 @@
-class removeElement {
+public class Solution {
     public int removeElement(int[] nums, int val) {
-        
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = s.removeElement(nums, val);
+
+        System.out.println("The new length of the array is: " + k);
     }
 }
